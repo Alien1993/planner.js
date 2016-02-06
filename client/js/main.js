ffb2b4ffe96ea8fad8744f2f24a6558735703993
Silvano Cerza <silvanocerza@gmail.com>
2016-02-06 14:23:39 +0100

    modified generateDateFromNow to set current month and selected day

--- client/js/main.js
+++ client/js/main.js
@@ -4,11 +4,11 @@
   // Auxiliar functions
   // ------------------
 
-  var generateDateFromNow = function (hours, minutes) {
+  var generateDateFromNow = function (day) {
     var now = new Date();
-    now.setHours(hours);
-    now.setMinutes(minutes);
-    now.setSeconds(0);
+    now.setDate(day);
+    now.setHours(0);
+    now.setMinutes(0);
 
     return now;
   };
@@ -21,8 +21,8 @@
       id: 1,
       title: 'Having a long dinner',
       content: 'Just a comment',
-      start: generateDateFromNow(19, 0),
-      end: generateDateFromNow(21, 45),
+      start: generateDateFromNow(2),
+      end: generateDateFromNow(5),
       columns: [1]
     });
 
@@ -30,27 +30,36 @@
       id: 2,
       title: 'DjangoCon meeting!',
       content: '@Orvieto',
-      start: generateDateFromNow(13, 0),
-      end: generateDateFromNow(15, 30),
-      columns: [1]
+      start: generateDateFromNow(5),
+      end: generateDateFromNow(14),
+      columns: [3]
     });
 
     var card3 = new Planner.Models.Card({
       id: 3,
       title: 'Hello IT',
       content: 'Standard nerds',
-      start: generateDateFromNow(13, 45),
-      end: generateDateFromNow(16, 15),
-      columns: [2, 4]
+      start: generateDateFromNow(13),
+      end: generateDateFromNow(15),
+      columns: [2]
     });
 
     var card4 = new Planner.Models.Card({
       id: 4,
       title: 'Having another dinner :(',
       content: 'Just a comment',
-      start: generateDateFromNow(19, 0),
-      end: generateDateFromNow(21, 45),
-      columns: [1]
+      start: generateDateFromNow(20),
+      end: generateDateFromNow(28),
+      columns: [0]
+    });
+
+    var card5 = new Planner.Models.Card({
+      id: 5,
+      title: 'GUH!',
+      content: 'What!?',
+      start: generateDateFromNow(4),
+      end: generateDateFromNow(14),
+      columns: [4]
     });
 
     // Planner options
